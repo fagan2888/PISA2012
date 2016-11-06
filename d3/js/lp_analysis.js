486b8fe0d06b99a145b80c720486684f9a524942
Tamara Makarova <toma.makarova@gmail.com>
2016-11-06 23:22:30 +0100

    Added right panel with dynamic bar chart

--- d3/js/lp_analysis.js
+++ d3/js/lp_analysis.js
@@ -1,8 +1,8 @@
-var margin = {top: 20, right: 40, bottom: 170, left: 80},
+var margin = {top: 60, right: 90, bottom: 170, left: 40},
     width = 1100 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-var right_margin = {top: 20, right: 15, bottom: 170, left: 15},
+var right_margin = {top: 60, right: 15, bottom: 170, left: 20},
     right_width = 300 - margin.left - margin.right,
     right_height = 400 - margin.top - margin.bottom;
 
@@ -16,16 +16,20 @@ var y = d3.scaleBand()
     .range([height, 0])
     .padding(0.1);
 
-
+var colors = ["steelblue", "white", "green"];
 
 var xAxis = d3.axisBottom(x);
-var yAxis = d3.axisLeft(y);
+var yAxis = d3.axisRight(y);
 
 var chart = d3.select(".chart.main")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var heatmap = chart.append("g")
+          .attr("class", "heatmap")
+          .on("mouseout", mout);
 
 var right_chart = d3.select(".chart.right")
   .attr("width", right_width + right_margin.left + right_margin.right)
@@ -35,8 +39,11 @@ var right_chart = d3.select(".chart.right")
                   + right_margin.top + ")");
 
 
-data = []
-all_data = []
+var data = [];
+var country_data = {};
+var aggregdata = [];
+//var bars;
+
 
 d3.csv("data/countries_stat_longreg.csv", type, function(error, rows) {
 
@@ -46,6 +53,15 @@ d3.csv("data/countries_stat_longreg.csv", type, function(error, rows) {
 
   rows.forEach(function (d) {
     countries.push(d.CNT)
+    country_data[d.CNT] = rows.columns.slice(2)
+                        .map(function (c) {
+                          return {
+                            key: c,
+                            value: d[c],
+                            country: d.CNT
+                          };
+                        });
+
     for (i = 0; i < features.length; i++) {
       key = features[i];
       data.push({
@@ -56,18 +72,24 @@ d3.csv("data/countries_stat_longreg.csv", type, function(error, rows) {
     }
   });
 
-  var aggdata = d3.nest()
+  aggregdata = d3.nest()
     .key(function (d) {return d.feature;})
     .rollup(function (values) {
-        return d3.mean(values, function (d) {return d.value} );
+        return d3.mean(values, function (d) {return d.value;});
       })
     .entries(data);
+
+  aggregdata = aggregdata.map(function (d) {
+    d["country"] = "All_countries";
+    return d;
+  })
+
 
   x.domain(countries);
   y.domain(features);
 
   render(data, features, countries);
-  render_right(aggdata);
+  render_right(aggregdata, "All_countries");
 
 });
 
@@ -78,13 +100,13 @@ function render (data) {
   var z = d3.scaleLinear()
       .domain([d3.min(data, function (d) {return d.value}), 0.0,
               d3.max(data, function (d) {return d.value})])
-      .range(["blue", "white", "red"]);
+      .range(colors);
 
   chart.append("g")
           .attr("class", "x axis");
 
   chart.append("g")
-          .attr("class", "y axis");
+          .attr("class", "y axis feature");
 
   chart.selectAll(".x.axis")
           .attr("transform", "translate(0," + (height + 10) + ")")
@@ -96,14 +118,14 @@ function render (data) {
           .attr("transform", "rotate(-75)")
           .style("text-anchor", "end");
 
-  chart.selectAll(".y.axis")
-        .attr("transform", "translate(0,0)")
+  chart.selectAll(".y.axis.feature")
+        .attr("transform", "translate(" + width + ",0)")
         .call(yAxis);
 
   var cardWidth = Math.floor(width / countries.length);
   var cardHeight = Math.floor(height / features.length);
 
-  var cards = chart.selectAll(".cards")
+  var cards = heatmap.selectAll(".cards")
           .data(data)
         .enter().append("rect")
           .attr("x", function(d) { return x(d.CNT);})
@@ -114,45 +136,116 @@ function render (data) {
           .attr("width", cardWidth)
           .attr("height", cardHeight)
           // .style("fill", function (d) {return z(d.value)});
-          .style("fill", "white");
+          .style("fill", "white")
+          .on("mouseover", mover);
+	        // .on("mouseout", mout);
 
     cards.transition().duration(1000)
             .style("fill", function(d) { return z(d.value); });
 
 
+    var defs = chart.append("defs");
+
+   defs.append("linearGradient")
+    	.attr("id", "gradient-colors")
+    	.attr("x1", "0%").attr("y1", "0%")
+    	.attr("x2", "100%").attr("y2", "0%")
+    	.selectAll("stop")
+    	.data(colors)
+    	.enter().append("stop")
+    	.attr("offset", function(d,i) { return i/(colors.length-1); })
+    	.attr("stop-color", function(d) { return d; });
+
+    var legendWidth = width * 0.6,
+        legendHeight = 10;
+
+    //Color Legend container
+    var legend = chart.append("g")
+        .attr("class", "legendWrapper")
+        .attr("transform", "translate(10,-30)");
+
+          //Draw the Rectangle
+    legend.append("rect")
+    	  .attr("class", "legendRect")
+    	  .attr("x", 10)
+    	  .attr("y", -10)
+    	    //.attr("rx", legendHeight/2)
+    	  .attr("width", legendWidth)
+    	  .attr("height", legendHeight)
+    	  .style("fill", "url(#gradient-colors)");
+
+    // legend.transition().duration(1000)
+    //     .style("fill", "url(#gradient-colors)");
 
 
+    //Append title
+    legend.append("text")
+  	  .attr("class", "legendTitle")
+  	  .attr("x", 10)
+  	  .attr("y", -15)
+  	  .text("Score");
+
+ //Set scale for x-axis
+  var xScaleLegend = d3.scaleLinear()
+  	   .range([0, legendWidth/2, legendWidth])
+  	   .domain([d3.min(data, function (d) {return d.value;}), 0,
+                d3.max(data, function (d) {return d.value;})]);
+  	 //.domain([d3.min(pt.legendSOM.colorData)/100, d3.max(pt.legendSOM.colorData)/100]);
+
+  //Define x-axis
+  var xAxisLegend = d3.axisBottom(xScaleLegend)
+  	  .ticks(8)  //Set rough # of ticks
+  	  //.tickFormat(formatPercent)
+
+  //Set up X axis
+  legend.append("g")
+  	.attr("class", "leg-axis")  //Assign "axis" class
+  	.attr("transform", "translate(10, 0)")
+  	.call(xAxisLegend);
 
 }
 
-function render_right(aggdata) {
+function mover(d) {
+  var country = d.CNT;
+  render_right(country_data[country], country)
+
+}
+
+function mout(d) {
+  render_right(aggregdata, "All_countries")
+}
+
+function render_right(aggdata, country) {
   var x_right = d3.scaleLinear()
       .range([0, right_width])
-      .domain([0,
-               d3.max(aggdata, function (d) {return d.value;})
-             ]).nice();
+      .domain([
+          Math.min(0, d3.min(aggdata, function (d) {return d.value;})),
+          d3.max(aggdata, function (d) {return d.value;})
+        ])
 
-  right_chart.append("g")
-     .attr("class", "x axis right");
+      .nice();
 
-  right_chart.selectAll(".x.axis.right")
-     .attr("transform", "translate(0," + (right_height + 10) + ")")
-     .call(d3.axisBottom(x_right));
+  var rbars = right_chart.selectAll(".bars")
+    .data(aggdata, function (d) {return d.key + ":" + clname(d.country) });
 
 
-  var bars = right_chart.selectAll(".bars.right")
-    .data(aggdata).enter()
-    .append("rect")
-    .attr("class", "bars right")
-    .attr("x", 0)
+  rbars.enter().append("rect")
+    .attr("class", function (d) { return "bars right " + clname(d.country);} )
+    .attr("x", function (d) {return x_right(Math.min(0, d.value));})
     .attr("y", function (d) {return y(d.key);})
-    .attr("width", function (d) {return x_right(d.value);})
+    .attr("width", function (d) {
+          return x_right(Math.abs(d.value)) - x_right(0) ;
+        })
     .attr("height", y.bandwidth())
     .style("fill", "grey")
-    .style("opacity", 0);
+    .style("opacity", 0.3);
 
-  bars.transition().duration(1000)
-          .style("opacity", 0.3);
+  rbars.exit().remove();
+
+
+
+  right_chart.selectAll(".right.label")
+      .remove();
 
   right_chart.append("text")
     .attr("class", "right label")
@@ -160,11 +253,29 @@ function render_right(aggdata) {
     .attr("y", -10)
     .style("text-anchor", "middle")
     .style("font", "13px sans-serif")
-    .text("Average Importance (all countries)");
+    .text(country);
+
+    right_chart.selectAll(".axis.right")
+        .remove();
+
+    right_chart.append("g")
+       .attr("class", "x axis right");
+
+    right_chart.selectAll(".x.axis.right")
+       .attr("transform", "translate(0," + (right_height + 10) + ")")
+   .call(d3.axisBottom(x_right));
+
+
 
 
 }
 
+
+function clname(name) {
+  return name.replace(/\s/g,"_")
+          .replace("(","")
+          .replace(")","");
+}
 
 function type(d) {
   return d;
